9814c1a60c9ff283b65562bc0a9ac6743618a42a
Như Võ Thị Tố <21520386@ms.uit.edu.vn>
2023-11-29 00:29:19 +0700

    update data

---
+++ SQL/QL_PMT_SP_Patient.sql
@@ -0,0 +1,59 @@
+﻿use QLPHONGMACHTU
+go
+
+-- viết các store procedure cho việc quản lí bệnh nhân 
+-- Lấp ra tất cả các bệnh nhân
+CREATE PROC spGetAllPatient
+as
+begin
+	select * from BENHNHAN
+end
+go
+
+-- Lấy ra các bệnh nhân chưa khám bệnh trong ngày khám truyền vào 
+CREATE PROC spGetPatientWithNgayKham
+@NgayKham smalldatetime
+as
+begin
+	select * from BENHNHAN
+	WHERE MaBN not in (SELECT MaBN from PHIEUKHAMBENH where DAY(NgayKham) = DAY(@NgayKham) and MONTH(NgayKham) = MONTH(@NgayKham) and YEAR(NgayKham) = YEAR(@NgayKham))
+end
+go
+
+-- Thêm một bệnh nhân mới vào data
+CREATE PROC spAddPatient
+@MaBN char(5),
+@HoTen nvarchar(40),
+@GioiTinh char(3),
+@NamSinh int,
+@DiaChi nvarchar(40)
+as
+begin
+	insert into BENHNHAN(MaBN, HoTen, GioiTinh, NamSinh, DiaChi) 
+	values (@MaBN, @HoTen, @GioiTinh, @NamSinh, @DiaChi)
+end
+go
+
+-- Xóa một bệnh nhân 
+CREATE PROC spDeletePatient
+@MaBN char(5)
+as
+begin
+	delete from BENHNHAN 
+	where MaBN = @MaBN
+end
+go
+
+-- Thay đổi thông tin bệnh nhân 
+CREATE PROC spUpdatePatient 
+@MaBN char(5),
+@HoTen nvarchar(40),
+@GioiTinh char(3),
+@NamSinh int,
+@DiaChi nvarchar(40)
+as
+begin
+	UPDATE BENHNHAN 
+	SET HoTen = @HoTen, GioiTinh = @GioiTinh , NamSinh = @NamSinh , DiaChi = @DiaChi
+	WHERE MaBN = @MaBN
+end
